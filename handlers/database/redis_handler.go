3d1440fdd48831b079e1623bc77336386aac77e6
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2024-07-15 10:12:12 +1200

    Fix redis handler not using TLS or auth

--- handlers/database/redis_handler.go
+++ handlers/database/redis_handler.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"crypto/tls"
 	"fmt"
 	"github.com/go-logr/logr"
 	"github.com/go-redis/redis"
@@ -13,15 +14,27 @@ func ReconcileRedis(database *taskv1.RedisDatabase, config configv1.RedisConfig,
 		return false, nil
 	}
 
+	var tlsConfig *tls.Config
+
+	if config.Spec.IsTlsEnabled != nil && *config.Spec.IsTlsEnabled {
+		tlsConfig = &tls.Config{}
+	}
+
 	logger.Info(fmt.Sprintf("Flushing redis database %d on connection %s", database.Spec.DatabaseNumber, config.Name))
 	client := redis.NewClient(
 		&redis.Options{
-			Addr: config.Spec.Host + ":" + fmt.Sprint(config.Spec.Port),
-			DB:   int(database.Spec.DatabaseNumber),
+			Addr:      config.Spec.Host + ":" + fmt.Sprint(config.Spec.Port),
+			DB:        int(database.Spec.DatabaseNumber),
+			Password:  config.Spec.Password,
+			TLSConfig: tlsConfig,
 		},
 	)
 
-	client.FlushDB()
+	foo := client.FlushDB()
+
+	if err := foo.Err(); nil != err {
+		return false, err
+	}
 
 	database.Status.State = taskv1.Complete
 
